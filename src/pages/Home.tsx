7fe1824cf1d4c5c71061f7236e7b181510f62bea
Frederik Kristensen <frede4947@gmail.com>
2026-01-24 14:35:54 +0100

    Placeholder for search action
    Now we save our form input and placeholder puts it in a alert

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import MovieCard from '../components/MovieCard.tsx';
+import { useState } from 'react';
 
 const Home = () => {
+  const [ searchQuery, setSearchQuery ] = useState(''); 
+
   const movies = [
     { id: 1, title: 'John Wick', release_date: '2012' },
     { id: 2, title: 'Inception', release_date: '2010' },
@@ -10,12 +13,24 @@ const Home = () => {
     { id: 6, title: 'Interstellar', release_date: '2014' },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+  };
 
   return (
     <div className="home">
       <form onSubmit={handleSearch} className="">
-        <input type="text" placeholder="Search for movies..." className="search-input" />
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
       <div className="movies-grid">
         {movies.map((movie) => (
