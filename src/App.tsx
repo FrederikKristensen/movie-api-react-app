fc5346689b78e29553a307b4404e7a0fd5685e28
Frederik Kristensen <frede4947@gmail.com>
2026-02-06 15:15:24 +0100

    Navbar set up
    Nav bar set up with our home and favorite pages.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import './css/App.css';
 import Favorites from './pages/Favorites.tsx';
 import Home from './pages/Home.tsx';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar.tsx';
 
 function App() {
   return (
     <>
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
+      <div>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </div>
     </>
   );
 }
