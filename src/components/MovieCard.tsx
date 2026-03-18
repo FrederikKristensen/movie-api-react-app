b75eb2115e35dbeb05f410c77185a92eed854d1e
Frederik Kristensen <frede4947@gmail.com>
2026-03-18 14:22:04 +0100

    Fixed favorite button
    Positioned in top left, shows only on hover on the specific card. Changes color on hover

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -7,14 +7,17 @@ const MovieCard = ({ movie }) => {
   // Class names are placeholder
   return (
     <div className="movie-card bg-neutral-800 text-white rounded-md">
-      <div className="movie-poster">
+      <div className="movie-poster relative group">
         <img
           className="rounded-md"
           src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
           alt={movie.title}
         />
         <div className="movie-overlay">
-          <button className="favorite-btn" onClick={onFavoriteClick}>
+          <button
+            className="favorite-btn absolute top-2 right-2 opacity-0 group-hover:opacity-100 hover:text-yellow-300 hover:bg-gray-500 rounded-full p-2 "
+            onClick={onFavoriteClick}
+          >
             ★
           </button>
         </div>
