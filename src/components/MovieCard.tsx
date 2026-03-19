e25b51eaabccc219b4fef498e0f7dd5f5950f4dd
Frederik Kristensen <frede4947@gmail.com>
2026-03-19 14:18:13 +0100

    Favorite function implemented
    Favorite page will not show favorited movies. Movie cards also will show by having a yellow star on the movies you have favorited already.

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,8 +1,16 @@
+import { useMovieContext } from "../context/Moviecontex";
+
 const MovieCard = ({ movie }) => {
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
   // alert is placeholder
-  function onFavoriteClick() {
-    alert('Clicked');
+  function onFavoriteClick(e) {
+    e.preventDefault();
+    if (favorite) removeFromFavorites(movie.id);
+    else addToFavorites(movie);
   }
+  // className="  "
 
   // Class names are placeholder
   return (
@@ -15,7 +23,8 @@ const MovieCard = ({ movie }) => {
         />
         <div className="movie-overlay">
           <button
-            className="favorite-btn absolute top-2 right-2 opacity-0 group-hover:opacity-100 hover:text-yellow-300 hover:bg-gray-500/40 rounded-full p-2 "
+            className={`favorite-btn absolute top-2 right-2 opacity-0 group-hover:opacity-100  hover:bg-gray-500/40 rounded-full p-2 
+              ${favorite ? 'active text-yellow-300 opacity-100' : 'hover:text-yellow-300'} `}
             onClick={onFavoriteClick}
           >
             ★
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,10 +1,27 @@
+import { useMovieContext } from '../context/Moviecontex';
+import MovieCard from '../components/MovieCard';
 const Favorites = () => {
-  return (
-    <div className="Favorites-empty">
-      <h2>No Favorite movies</h2>
-      <p>Favorite movies and they will be here</p>
-    </div>
-  );
+  const { favorites } = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your favorites</h2>
+        <div className="movies-grid grid grid-cols-6 gap-4">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Favorites-empty">
+        <h2>No Favorite movies</h2>
+        <p>Favorite movies and they will be here</p>
+      </div>
+    );
+  }
 };
 
 export default Favorites;
